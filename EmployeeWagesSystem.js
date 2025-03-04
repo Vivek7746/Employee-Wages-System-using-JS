a7b93c71534933b3914fb632c6ed8cfce8d7fc0a
Vivek <vivekthegamer999@gmail.com>
2025-03-04 12:05:07 +0530

    [Vivek Yadav] Add . Calculating Wages of Employees

--- EmployeeWagesSystem.js
+++ EmployeeWagesSystem.js
@@ -1,19 +1,3 @@
-function getWorkingHours(empCheck) {
-    switch(empCheck) {
-        case isPartTime:
-            empHrs = PartTimeHours;
-            break;
-        case isFullTime:
-            empHrs = FullTimeHours;
-            break;
-        default:
-            empHrs = 0;
-    }
-    
-    let empWage = empHrs * WagesPerHour;
-    return empWage;
-}
-
 const isPartTime = 1;
 const isFullTime = 2;
 const PartTimeHours = 4;
@@ -24,6 +8,17 @@ let empHrs = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 3;
 
-let empWage = getWorkingHours(empCheck);
+switch(empCheck) {
+        case isPartTime:
+            empHrs = PartTimeHours;
+            break;
+        case isFullTime:
+            empHrs = FullTimeHours;
+            break;
+        default:
+            empHrs = 0;
+    }
+    
+let empWage = empHrs * WagesPerHour;
 
 console.log("Employee's Wage: " + empWage);
