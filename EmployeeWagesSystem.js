14755fd847ea9c1921ab96e01d20570e03a05798
Vivek <vivekthegamer999@gmail.com>
2025-03-04 11:54:58 +0530

    [Vivek Yadav] Add . Finding Wages using a Function

--- EmployeeWagesSystem.js
+++ EmployeeWagesSystem.js
@@ -1,3 +1,19 @@
+function getWorkingHours(empCheck) {
+    switch(empCheck) {
+        case isPartTime:
+            empHrs = PartTimeHours;
+            break;
+        case isFullTime:
+            empHrs = FullTimeHours;
+            break;
+        default:
+            empHrs = 0;
+    }
+    
+    let empWage = empHrs * WagesPerHour;
+    return empWage;
+}
+
 const isPartTime = 1;
 const isFullTime = 2;
 const PartTimeHours = 4;
@@ -7,19 +23,7 @@ const WagesPerHour = 20;
 let empHrs = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 3;
-console.log("empCheck: " + empCheck);
-switch(empCheck) {
-    case isPartTime:
-        empHrs = PartTimeHours;
-        break;
-    case isFullTime:
-        empHrs = FullTimeHours;
-        break;
-    default:
-        empHrs = 0;
-}
 
-console.log("empHrs: " + empHrs);
+let empWage = getWorkingHours(empCheck);
 
-let empWage = empHrs * WagesPerHour;
 console.log("Employee's Wage: " + empWage);
